a3d11ee4952f31a84891dd6923bbbe9833295376
Toñin <105400266+gatanothoa@users.noreply.github.com>
2022-12-07 11:35:16 -0600

    Practicando commits

--- JS/Piedra.js
+++ JS/Piedra.js
@@ -1,6 +1,6 @@
 // 1 Es piedra, 2 es papel y 3 es tijera
 let jugador = 0
-let pc = 3
+let pc = Math.ceil(Math.random()*3)
 
 jugador = prompt("Elije 1 para diamante, 2 para papel, 3 para tijera ")
 // alert("Elegiste " + jugador)
@@ -29,6 +29,10 @@ jugador = prompt("Elije 1 para diamante, 2 para papel, 3 para tijera ")
         alert("EMPATE")
     } else if (jugador == 1 && pc == 3){
         alert("GANASTE")
-        
+    } else if (jugador == 2 && pc == 1){
+        alert("GANASTE")
+    } else if (jugador == 3 && pc == 2){
+        alert("GANASTE")
+    } else {
+        alert("PERDISTE")
     }
-
