90dbbbd1f2641b8b56d633583c79dfc6116b6e0a
Toñin <105400266+gatanothoa@users.noreply.github.com>
2022-12-07 10:47:09 -0600

    Nuevo js
    subiendo piedra, papel o tijera

---
+++ JS/Piedra.js
@@ -0,0 +1,34 @@
+// 1 Es piedra, 2 es papel y 3 es tijera
+let jugador = 0
+let pc = 3
+
+jugador = prompt("Elije 1 para diamante, 2 para papel, 3 para tijera ")
+// alert("Elegiste " + jugador)
+
+    if (jugador == 1){
+    alert("Elegiste 💎")    
+    } else if (jugador == 2){
+        alert("Elegiste 🧻")      
+    } else if (jugador == 3){
+        alert("Elegiste ✂")      
+    } else{
+        alert("Elegiste PERDER")
+    }
+
+        if (pc == 1){
+        alert("PC elige 💎")    
+        } else if (pc == 2){
+            alert("PC elige  🧻")      
+        } else if (pc == 3){
+            alert("PC elige  ✂")      
+        }
+
+    //COMBATE
+
+    if (pc == jugador){
+        alert("EMPATE")
+    } else if (jugador == 1 && pc == 3){
+        alert("GANASTE")
+        
+    }
+
